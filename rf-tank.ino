fad97bc03f03d61024f285817916d6857086b0db
Sergiu Falcusan <sergiu@falcusan.ro>
2017-11-15 20:17:17 +0200

    change wifi pipes

--- rf-tank.ino
+++ rf-tank.ino
@@ -4,23 +4,23 @@
 #include <RF24.h>
 
 RF24 radio(7, 8); // CE, CSN
-
-const byte address[6] = "00001";
-
-
 Servo escRight, escLeft;
 
+const uint64_t pipes[2] = {
+  0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+  
 const int escRightPin = 9;
 const int escLeftPin = 10;
 
 void setup() {
   // init radio pins
-  Serial.begin(9600);
+  Serial.begin(57600);
+  Serial.println("INIT");
   radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
+  radio.setRetries(15,15);
+  radio.openReadingPipe(0,pipes[0]);
   radio.startListening();
-
+  radio.printDetails();  
   // init esc motor pins
   // escRight.attach(escRightPin); //ESC Pin
   // escRight.write(86);
@@ -32,12 +32,10 @@ void loop() {
   /**
    * Example radio read
    */
-   
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
+
+  char data[12];   
+  radio.read(&data, sizeof(data)); 
+  Serial.print("Received: "); Serial.println(data);
   
   /*
    * Example esc write
